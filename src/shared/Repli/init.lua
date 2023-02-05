a56bf974890a9169ed68a0d42611de2c2385d8ff
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-02-04 22:17:52 -0500

    Update init.lua

--- src/shared/Repli/init.lua
+++ src/shared/Repli/init.lua
@@ -1,5 +1,6 @@
 local RunService = game:GetService("RunService");
 
+-- Require RepliClient or RepliServer depending on the environment
 if (RunService:IsClient()) then
     return require(script.RepliClient);
 else
