60037e718728826002f51cfc90c4b9694b449d67
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-02-06 16:38:52 -0500

    Updated main repli test files

--- src/client/RepliClientTest.client.lua
+++ src/client/RepliClientTest.client.lua
@@ -6,8 +6,8 @@ local Repli = require(Common:WaitForChild("Repli"));
 local PlayerGui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui");
 local ScreenGui = PlayerGui:WaitForChild("ScreenGui");
 
--- local TestPlayerData = Repli.fromValue("TestPlayerData");
--- local Test2 = Repli.fromValue("Test2");
+local TestPlayerData = Repli.fromValue("TestPlayerData");
+local Test2 = Repli.fromValue("Test2");
 
 -- Subscribe to it and print the value, and also update the text label
 local function subscribedFunctionTestPlayerdData(value)
@@ -16,14 +16,14 @@ local function subscribedFunctionTestPlayerdData(value)
     print("Player gems:", value.Gems);
     ScreenGui.TextLabel.Text = "Gems: " .. value.Gems;
 end
--- TestPlayerData:subscribe(subscribedFunctionTestPlayerdData);
+TestPlayerData:subscribe(subscribedFunctionTestPlayerdData);
 
--- local function subscribedFunctionTest2(value)
---     print("Client subscribed to Test2: ", value);
--- end
--- Test2:subscribe(subscribedFunctionTest2);
+local function subscribedFunctionTest2(value)
+    print("Client subscribed to Test2: ", value);
+end
+Test2:subscribe(subscribedFunctionTest2);
 
 -- TextButton is a button that fires the event
--- ScreenGui:WaitForChild("TextButton").MouseButton1Click:Connect(function()
---     ReplicatedStorage.RemoteEvent:FireServer();
--- end)
+ScreenGui:WaitForChild("TextButton").MouseButton1Click:Connect(function()
+    ReplicatedStorage.RemoteEvent:FireServer();
+end)
